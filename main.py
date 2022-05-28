c28766e8578ea90f8a37c4671bb772d272fd0e4e
jingtongsh <jingtongsh@gmail.com>
2022-05-28 10:04:13 +0800

    nearly done

--- main.py
+++ main.py
@@ -3,117 +3,46 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
-from torch.distributions.multivariate_normal import MultivariateNormal
 from torch.utils.tensorboard import SummaryWriter
 import os
 import time
-
-
-class Encoder(nn.Module):
-    def __init__(self, out_shape, device):
-        super(Encoder, self).__init__()
-        self.out_shape = out_shape
-        self.net = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=1, kernel_size=5),
-            # nn.BatchNorm2d(num_features=1),
-            nn.LeakyReLU(),
-            nn.Conv2d(in_channels=1, out_channels=1, kernel_size=5),
-            # nn.BatchNorm2d(num_features=1),
-            nn.LeakyReLU(),
-            nn.Flatten(),
-            nn.Linear(in_features=400, out_features=200),
-            nn.LeakyReLU(),
-            nn.Linear(in_features=200, out_features=out_shape * 2)
-        )
-        self.net.to(device)
-
-    def forward(self, x):
-        batch_size = x.shape[0]
-        out = self.net(x).view(batch_size, 2, self.out_shape)
-        mu = out[:, 0, :]
-        sig = out[:, 1, :]
-        return mu, sig
-
-
-class Decoder(nn.Module):
-    def __init__(self, in_shape, device):
-        super(Decoder, self).__init__()
-        self.fc = nn.Sequential(
-            nn.Linear(in_features=in_shape, out_features=200),
-            nn.LeakyReLU(),
-            nn.Linear(in_features=200, out_features=400),
-            nn.LeakyReLU()
-        )
-        self.ct = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=1, out_channels=1, kernel_size=5),
-            # nn.BatchNorm2d(num_features=1),
-            nn.LeakyReLU(),
-            nn.ConvTranspose2d(in_channels=1, out_channels=1, kernel_size=5),
-            nn.Sigmoid()
-        )
-        self.fc.to(device)
-        self.ct.to(device)
-
-    def forward(self, z):
-        z = self.fc(z)
-        z = z.view(z.shape[0], 1, 20, 20)
-        out = self.ct(z)
-        return out
-
-    def generate(self, z):
-        out = self.forward(z)
-        # out = (out > 0.5).float()
-        return out
-
-
-class VAE(nn.Module):
-    def __init__(self, latent_shape, device):
-        super(VAE, self).__init__()
-        self.latent_shape = latent_shape
-        self.device = device
-        self.enc = Encoder(out_shape=latent_shape, device=device)
-        self.dec = Decoder(in_shape=latent_shape, device=device)
-        self.gaussian = MultivariateNormal(loc=torch.zeros(self.latent_shape),
-                                           covariance_matrix=torch.eye(self.latent_shape))
-
-    def forward(self, x):
-        mu, sig = self.enc(x)
-        ep = self.gaussian.sample(sample_shape=x.shape[0: 1]).to(self.device)
-        latent = mu + sig * ep
-        out = self.dec(latent)
-        return mu, sig, out
-
-    def generate_from_img(self, x):
-        _, _, out = self.forward(x)
-        # out = (out > 0.5).float()
-        return out
-
-    def generate_from_latent(self, z):
-        out = self.dec(z)
-        return out
 
 
 class SimpleVAE(nn.Module):
     def __init__(self, latent_shape):
         super(SimpleVAE, self).__init__()
         self.enc = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(in_features=28 ** 2, out_features=200),
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5),
             nn.Tanh(),
-            nn.Linear(in_features=200, out_features=latent_shape)
+            nn.Conv2d(in_channels=6, out_channels=6, kernel_size=5),
+            nn.Tanh(),
+            nn.Conv2d(in_channels=6, out_channels=6, kernel_size=3),
+            nn.Tanh(),
+            nn.Flatten(),
+            nn.Linear(in_features=6*18*18, out_features=1200),
+            nn.Tanh(),
+            nn.Linear(in_features=1200, out_features=latent_shape)
         )
-        self.dec = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(in_features=latent_shape, out_features=200),
+        self.dec_fc = nn.Sequential(
+            nn.Linear(in_features=latent_shape, out_features=1200),
             nn.Tanh(),
-            nn.Linear(in_features=200, out_features=28 ** 2),
-            nn.Tanh()
+            nn.Linear(in_features=1200, out_features=6*18*18),
+            nn.Tanh(),
+        )
+        self.dec_cv = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=6, out_channels=6, kernel_size=3),
+            nn.Tanh(),
+            nn.ConvTranspose2d(in_channels=6, out_channels=6, kernel_size=5),
+            nn.Tanh(),
+            nn.ConvTranspose2d(in_channels=6, out_channels=1, kernel_size=5),
+            nn.Tanh(),
         )
 
     def forward(self, x):
         z = self.enc(x)
-        out = self.dec(z)
-        out = out.view(-1, 1, 28, 28)
+        temp = self.dec_fc(z)
+        temp = temp.view(z.shape[0], 6, 18, 18)
+        out = self.dec_cv(temp).view(z.shape[0], 1, 28, 28)
         return z, out
 
     def generate_from_img(self, x):
@@ -126,15 +55,24 @@ class SimpleVAE(nn.Module):
         return out
 
 
+def simple_log(f, info):
+    f.write(info + '\n')
+    print(info)
+
+
 def main():
     # Arguments
     batch_size = 32
     device = 'cuda:0'
-    epochs = 40
+    epochs = 10
     latent_shape = 2
     learning_rate = 1e-3
-    log_dir = './'
-    save_dir = './'
+    model_save_dir = './model'
+
+    if not os.path.isdir(model_save_dir):
+        os.mkdir(model_save_dir)
+
+    log_file = open('log.txt', 'w')
 
     # Download MNIST Dataset
     train_dataset = datasets.MNIST(root='./data/', train=True, transform=transforms.ToTensor(), download=True)
@@ -147,7 +85,7 @@ def main():
     # Initialize
     model = SimpleVAE(latent_shape=latent_shape)
     model.to(device)
-    # writer = SummaryWriter(log_dir=log_dir)
+    writer = SummaryWriter(log_dir='./')
     optim = torch.optim.SGD(params=model.parameters(), lr=learning_rate)
 
     # Train
@@ -163,6 +101,8 @@ def main():
             latent, out = model.forward(data)
             mu = latent.mean(dim=0)
             sig = latent.T.cov()
+            if sig.ndim < 2:
+                sig = sig.view(1, 1)
             kl_div = (-torch.log(sig.det()) - latent.shape[-1] + sig.trace() + mu.matmul(mu)) / 2
             rec_loss = F.mse_loss(out, data)
             loss = kl_div + rec_loss
@@ -171,25 +111,26 @@ def main():
 
             epoch_loss += torch.Tensor([kl_div, rec_loss], device='cpu')
 
-            if batch_idx == 0:
-                print('mean\n', latent.mean())
-                print('cov\n', latent.T.cov())
-                print('det\n', latent.T.cov().det())
-            # writer.add_scalar('regularization', loss1, batch_idx)
-            # writer.add_scalar('reconstruction', loss2, batch_idx)
-            # writer.add_scalar('total', loss, batch_idx)
+        ep_kld, ep_rec = epoch_loss
+        ep_loss = epoch_loss.sum()
+        writer.add_scalar('KL', ep_kld, epoch)
+        writer.add_scalar('reconstruction', ep_rec, epoch)
+        writer.add_scalar('total', ep_loss, epoch)
 
         duration = time.time() - start
-        print('| epoch {} kl divergence {:.2f} | rec loss {:.2f} | total loss {:.2f} | training time {:.2f} s |'
-              .format(epoch, epoch_loss[0], epoch_loss[1], epoch_loss.sum(), duration))
+        log_info = '| epoch {} | kl divergence {:.2f} | rec loss {:.2f} | total loss {:.2f} | training time {:.2f} s |'\
+                   .format(epoch, ep_kld, ep_rec, ep_loss, duration)
+        simple_log(log_file, log_info)
 
         if epoch % 5 == 0:
             print('Testing ...')
             start = time.time()
-            test_dir = f'./test_images_{epoch}'
+            test_dir = f'./test_images_{latent_shape}_{epoch}'
             if not os.path.isdir(test_dir):
                 os.mkdir(test_dir)
             model.eval()
+
+            # generate and save images
             for batch_idx, (data, _) in enumerate(test_loader):
                 data = data.to(device)
                 images = model.generate_from_img(data)
@@ -198,7 +139,11 @@ def main():
                            os.path.join(test_dir, img_name),
                            nrow=8)
             duration = time.time() - start
-            print('images saved to {}/; test time {:.2f} s'.format(test_dir, duration))
+            log_info = 'generated test images saved to {}/; test time {:.2f} s'.format(test_dir, duration)
+            simple_log(log_file, log_info)
+
+            # save model
+            torch.save(model, os.path.join(model_save_dir, f'model_{latent_shape}_{epoch}.pt'))
 
 
 if __name__ == '__main__':
